f9a386ba7f2bfc57bfa7086ed59711786e9b201a
Nukedeggz <sethmegg@yahoo.com>
2025-02-27 23:20:07 -0500

    imp middle for par json

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('../client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
